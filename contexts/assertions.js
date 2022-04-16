369ba3ce95720f72ccd624dc17901ab8026bd919
TasnimNatsheh <t.natshah@gmail.com>
2022-04-16 15:24:04 +0300

    Change the I should see step defenition to use body HTML element #2

--- contexts/assertions.js
+++ contexts/assertions.js
@@ -2,5 +2,5 @@ const { client } = require('nightwatch');
 const { Given, When, Then, And, But } = require('cucumber');
 
 Then(/^I should see "([^"]*)"$/, textValue => {
-  return client.assert.containsText(elementCss, textValue);
+  return client.assert.containsText('body', textValue);
 });
