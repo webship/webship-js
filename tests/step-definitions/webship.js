46aca0d8cd914e192041cdac15a7aee6ec94e523
Tasneem Natshah <t.natshah@gmail.com>
2022-10-25 21:33:22 +0300

    Linting JavaScript and no extension node files using ESLint linter #29

--- tests/step-definitions/webship.js
+++ tests/step-definitions/webship.js
@@ -1,21 +1,17 @@
-const {Given} = require('@cucumber/cucumber');
-const {When, Before} = require('@cucumber/cucumber');
-const {Then} = require('@cucumber/cucumber');
+const { Given } = require('@cucumber/cucumber');
+const { When, Before } = require('@cucumber/cucumber');
+const { Then } = require('@cucumber/cucumber');
 
 /**
  * some description
  * for first given function
  */
-Given(/^I go to "([^"]*)?"$/, function(url) {
-  return browser.url(url);
-});
+Given(/^I go to "([^"]*)?"$/, (url) => browser.url(url));
 
-Then(/^I should( not)* see "([^"]*)?"$/, function(negativeCase, expectedText) {
+Then(/^I should( not)* see "([^"]*)?"$/, (negativeCase, expectedText) => {
   if (negativeCase) {
     return browser.assert.not.textContains('body', expectedText);
   }
-  
+
   return browser.assert.textContains('body', expectedText);
 });
-
-
