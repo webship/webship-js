32c187d57f67724810d001b48198b2a7b322f681
Tasneem Natshah <t.natshah@gmail.com>
2022-11-01 01:05:40 +0200

    Relase webship-js 1.0.6 #30

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -33,65 +33,65 @@ module.exports = {
     options: {
       feature_path: 'tests/features/*.feature',
       additional_config: '',
-      parallel: 2
-    }
-   
+      parallel: 2,
+    },
+
   },
 
   test_settings: {
-      default: {
-        launch_url: "http://webship.co",
-        selenium_port: 4444,
-        selenium_host: "127.0.0.1",
-        silent: true,
-        screenshots: {
-            enabled: true,
-            path: "./reports/screenshots"
-        },
-        
-        desiredCapabilities: {
-          browserName: 'chrome',
-          'goog:chromeOptions': {
-            // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
-            //
-            // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
-            w3c: false,
-            args: [
-              '--headless',
-              '--start-maximized',
-              '--disable-gpu',
-              '--window-size=1600,1200',
-              '--no-sandbox',
-              '--disable-dev-shm-usage',
-              '--disable-setuid-sandbox',
-              '--disable-web-security',
-              '--DNS-prefetch-disable',
-              '--disable-translate',
-              '--ignore-certificate-errors',
-              '--test-type',
-              '--disable-extensions',
-              '--incognito',
-              '--disable-infobars',
-              '--remote-debugging-port=9222',
-              '--allowed-ips=*',
-              '--whitelisted-ips=*',
-              '--allow-insecure-localhost'
-            ]
-          }
-        }
+    default: {
+      launch_url: 'http://webship.co',
+      selenium_port: 4444,
+      selenium_host: '127.0.0.1',
+      silent: true,
+      screenshots: {
+        enabled: true,
+        path: './reports/screenshots',
       },
+
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+          //
+          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
+          w3c: false,
+          args: [
+            '--headless',
+            '--start-maximized',
+            '--disable-gpu',
+            '--window-size=1600,1200',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--disable-web-security',
+            '--DNS-prefetch-disable',
+            '--disable-translate',
+            '--ignore-certificate-errors',
+            '--test-type',
+            '--disable-extensions',
+            '--incognito',
+            '--disable-infobars',
+            '--remote-debugging-port=9222',
+            '--allowed-ips=*',
+            '--whitelisted-ips=*',
+            '--allow-insecure-localhost',
+          ],
+        },
+      },
+    },
 
     safari: {
       desiredCapabilities: {
         browserName: 'safari',
         alwaysMatch: {
-          acceptInsecureCerts: false
-        }
+          acceptInsecureCerts: false,
+        },
       },
       webdriver: {
         start_process: true,
-        server_path: ''
-      }
+        server_path: '',
+      },
     },
 
     firefox: {
@@ -103,9 +103,9 @@ module.exports = {
             args: [
               // '-headless',
               // '-verbose'
-            ]
-          }
-        }
+            ],
+          },
+        },
       },
       webdriver: {
         start_process: true,
@@ -113,8 +113,8 @@ module.exports = {
         cli_args: [
           // very verbose geckodriver logs
           // '-vv'
-        ]
-      }
+        ],
+      },
     },
 
     chrome: {
@@ -126,12 +126,12 @@ module.exports = {
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: true,
           args: [
-            //'--no-sandbox',
-            //'--ignore-certificate-errors',
-            //'--allow-insecure-localhost',
-            //'--headless'
-          ]
-        }
+            // '--no-sandbox',
+            // '--ignore-certificate-errors',
+            // '--allow-insecure-localhost',
+            // '--headless'
+          ],
+        },
       },
 
       webdriver: {
@@ -139,8 +139,8 @@ module.exports = {
         server_path: '',
         cli_args: [
           // --verbose
-        ]
-      }
+        ],
+      },
     },
 
     edge: {
@@ -150,9 +150,9 @@ module.exports = {
           w3c: true,
           // More info on EdgeDriver: https://docs.microsoft.com/en-us/microsoft-edge/webdriver-chromium/capabilities-edge-options
           args: [
-            //'--headless'
-          ]
-        }
+            // '--headless'
+          ],
+        },
       },
 
       webdriver: {
@@ -162,16 +162,16 @@ module.exports = {
         server_path: '',
         cli_args: [
           // --verbose
-        ]
-      }
+        ],
+      },
     },
 
-    //////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////
     // Configuration for when using cucumber-js (https://cucumber.io)                |
     //                                                                               |
     // It uses the bundled examples inside the nightwatch examples folder; feel free |
     // to adapt this to your own project needs                                       |
-    //////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////
     'cucumber-js': {
       src_folders: ['examples/cucumber-js/features/step_definitions'],
 
@@ -181,56 +181,56 @@ module.exports = {
 
         // define cucumber specific options
         options: {
-          //set the feature path
-          feature_path: 'node_modules/nightwatch/examples/cucumber-js/*/*.feature'
+          // set the feature path
+          feature_path: 'node_modules/nightwatch/examples/cucumber-js/*/*.feature',
 
           // start the webdriver session automatically (enabled by default)
           // auto_start_session: true
 
           // use parallel execution in Cucumber
           // parallel: 2 // set number of workers to use (can also be defined in the cli as --parallel 2
-        }
-      }
+        },
+      },
     },
 
-    //////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////
     // Configuration for when using the browserstack.com cloud service               |
     //                                                                               |
     // Please set the username and access key by setting the environment variables:  |
     // - BROWSERSTACK_USER                                                           |
     // - BROWSERSTACK_KEY                                                            |
     // .env files are supported                                                      |
-    //////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////
     browserstack: {
       selenium: {
         host: 'hub-cloud.browserstack.com',
-        port: 443
+        port: 443,
       },
       // More info on configuring capabilities can be found on:
       // https://www.browserstack.com/automate/capabilities?tag=selenium-4
       desiredCapabilities: {
         'bstack:options': {
           userName: '${BROWSERSTACK_USER}',
-          accessKey: '${BROWSERSTACK_KEY}'
-        }
+          accessKey: '${BROWSERSTACK_KEY}',
+        },
       },
 
       disable_error_log: true,
       webdriver: {
         timeout_options: {
           timeout: 15000,
-          retry_attempts: 3
+          retry_attempts: 3,
         },
         keep_alive: true,
-        start_process: false
-      }
+        start_process: false,
+      },
     },
 
     'browserstack.local': {
       extends: 'browserstack',
       desiredCapabilities: {
-        'browserstack.local': true
-      }
+        'browserstack.local': true,
+      },
     },
 
     'browserstack.chrome': {
@@ -238,50 +238,50 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome',
         chromeOptions: {
-          w3c: true
-        }
-      }
+          w3c: true,
+        },
+      },
     },
 
     'browserstack.firefox': {
       extends: 'browserstack',
       desiredCapabilities: {
-        browserName: 'firefox'
-      }
+        browserName: 'firefox',
+      },
     },
 
     'browserstack.ie': {
       extends: 'browserstack',
       desiredCapabilities: {
         browserName: 'internet explorer',
-        browserVersion: '11.0'
-      }
+        browserVersion: '11.0',
+      },
     },
 
     'browserstack.safari': {
       extends: 'browserstack',
       desiredCapabilities: {
-        browserName: 'safari'
-      }
+        browserName: 'safari',
+      },
     },
 
     'browserstack.local_chrome': {
       extends: 'browserstack.local',
       desiredCapabilities: {
-        browserName: 'chrome'
-      }
+        browserName: 'chrome',
+      },
     },
 
     'browserstack.local_firefox': {
       extends: 'browserstack.local',
       desiredCapabilities: {
-        browserName: 'firefox'
-      }
+        browserName: 'firefox',
+      },
     },
-    //////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////
     // Configuration for when using the Selenium service, either locally or remote,  |
     //  like Selenium Grid                                                           |
-    //////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////
     selenium_server: {
       // Selenium Server is running locally and is managed by Nightwatch
       selenium: {
@@ -290,9 +290,9 @@ module.exports = {
         server_path: (Services.seleniumServer ? Services.seleniumServer.path : ''),
         cli_args: {
           'webdriver.gecko.driver': (Services.geckodriver ? Services.geckodriver.path : ''),
-          'webdriver.chrome.driver': (Services.chromedriver ? Services.chromedriver.path : '')
-        }
-      }
+          'webdriver.chrome.driver': (Services.chromedriver ? Services.chromedriver.path : ''),
+        },
+      },
     },
 
     'selenium.chrome': {
@@ -300,9 +300,9 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome',
         chromeOptions: {
-          w3c: true
-        }
-      }
+          w3c: true,
+        },
+      },
     },
 
     'selenium.firefox': {
@@ -313,11 +313,11 @@ module.exports = {
           args: [
             // '-headless',
             // '-verbose'
-          ]
-        }
-      }
-    }
-  }
+          ],
+        },
+      },
+    },
+  },
 };
 
 function loadServices() {
