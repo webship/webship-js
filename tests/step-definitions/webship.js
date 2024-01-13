382059731c4edde7b51818a653be74e12f523cf3
Rajab Natshah <rajabn@gmail.com>
2024-01-13 21:19:31 +0300

    Switch back again to Nightwatch ~3 on the 1.0.x branch #119

--- tests/step-definitions/webship.js
+++ tests/step-definitions/webship.js
@@ -1,7 +1,6 @@
 const { Given } = require('@cucumber/cucumber');
 const { When, Before } = require('@cucumber/cucumber');
 const { Then } = require('@cucumber/cucumber');
-const { endsWith } = require('lodash');
 
 const request = require('request');
 
