6f3ef12e74cf371102610d3172fc89cd9aecffe2
Tasneem Natshah <t.natshah@gmail.com>
2025-05-31 16:20:24 +0300

    Add global variables to specify minimum wait durations #205

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -24,8 +24,6 @@ module.exports = {
     assets_folder : "/assets/",
     min_wait_time: {
       page : 3000,
-      before_scenario : 0,
-      after_scenario : 0,
       before_step : 0,
       after_step : 1000
     }
