b726e07b50235222c31baa0b096780b07d004494
Tasneem Natshah <t.natshah@gmail.com>
2026-03-05 18:12:26 +0300

    Start a 2.0.x branch and switch all step definitions to Playwright #241

--- nightwatch.conf.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['./tests/step-definitions'],
-};
